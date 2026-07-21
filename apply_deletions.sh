126054299213c658669abbe8a2549ed8980b0ee4
ErenalpCet <122112517+ErenalpCet@users.noreply.github.com>
2026-07-21 19:32:32 +0000

    Initial commit: LineageOS 23.2 GSI custom patches and prebuilts

---
+++ apply_deletions.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script to delete files removed in custom GSI build
+
+rm -f "packages/apps/Dialer/Android.bp"
+rm -f "packages/apps/Messaging/Android.bp"
---
+++ vendor/lineage/prebuilt/common/etc/idc/sec_e-pen.idc
@@ -0,0 +1,4 @@
+device.internal = 1
+touch.deviceType = stylus
+touch.orientationAware = 1
+
